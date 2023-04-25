0c6dc6f5dd85a5a9e8ea1f5a3431a2b0dd4e2c4c
massimobiagioli <biagiolimassimo@gmail.com>
2023-04-25 09:23:36 +0200

    first commit

---
+++ src/main/java/com/github/massimobiagioli/sbbacktobasics/SbBackToBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.github.massimobiagioli.sbbacktobasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBackToBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBackToBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/massimobiagioli/sbbacktobasics/SbBackToBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.massimobiagioli.sbbacktobasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBackToBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
